bad41e358791201bbbaf1feaa4a85025d1cbf0e8
tonylixu@gmail.com <tonylixu@gmail.com>
2017-03-26 10:20:42 -0400

    Created a ReaderRepository Interface

---
+++ src/main/java/com/zebra/emc/tools/ReadingList/ReaderRepository.java
@@ -0,0 +1,10 @@
+package com.zebra.emc.tools.ReadingList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Tony Li Xu on 3/26/2017.
+ */
+public interface ReaderRepository extends JpaRepository<Reader, String>{
+
+}
