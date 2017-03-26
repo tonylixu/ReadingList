7a8b47b47ab079bd2fcab1e92f8a71b0a543fbc6
tonylixu@gmail.com <tonylixu@gmail.com>
2017-03-26 10:22:47 -0400

    Comments to ReaderRepository

--- src/main/java/com/zebra/emc/tools/ReadingList/ReaderRepository.java
+++ src/main/java/com/zebra/emc/tools/ReadingList/ReaderRepository.java
@@ -3,7 +3,8 @@ package com.zebra.emc.tools.ReadingList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * Created by Tony Li Xu on 3/26/2017.
+ * Since this extends JpaRepository, there is no need to write and implementation of
+ * ReaderRepository. Spring Data JPA will automatically create an implementation of its runtime.
  */
 public interface ReaderRepository extends JpaRepository<Reader, String>{
 
