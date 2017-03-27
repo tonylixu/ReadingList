b3aec59c47a1e58a7031dd1943a92d38cea0ee1b
tonylixu@gmail.com <tonylixu@gmail.com>
2017-03-27 10:57:34 -0400

    Added setupMockMvc method which runs before any test methods.

--- src/test/java/com/zebra/emc/tools/ReadingList/MockMvcWebTests.java
+++ src/test/java/com/zebra/emc/tools/ReadingList/MockMvcWebTests.java
@@ -1,9 +1,14 @@
 package com.zebra.emc.tools.ReadingList;
 
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * @RunWith: Given SpringJUnit4ClassRunner.class to enable Spring integration context, here we
@@ -11,11 +16,29 @@ import org.springframework.test.context.web.WebAppConfiguration;
  * MockMvcWebTests
  * @EnableAutoConfiguration: Since SpringApplicationConfiguration is deprecated as of 1.4, we use
  * EnableAutoConfiguration instead
- * @
+ * @WebAppConfiguration declares that the application context created by SpringJUnit4ClassRunner
+ * should be a WebApplicationContext (as opposed to a basic non-web ApplicationContext).
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @EnableAutoConfiguration
 @WebAppConfiguration
 public class MockMvcWebTests {
+
+    @Autowired
+    private WebApplicationContext webContext;
+
+    private MockMvc mockMvc;
+
+    /*
+     * This setupMockMvc method should be executed before any test methods. The injected
+     * WebApplicationContext has been passed into the webAppContextSetup() method and then calls
+     * build() to produce a MockMvc instance.
+     */
+    @Before
+    public void setupMockMvc() {
+        mockMvc = MockMvcBuilders
+            .webAppContextSetup(webContext)
+            .build();
+    }
 
 }
